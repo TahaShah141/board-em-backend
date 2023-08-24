7c5cf51c0afecd680f377e4c83f6e871a20d7a5a
Taha Shah <tahashah411@gmail.com>
2023-08-24 23:19:51 +0500

    finishing touches

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -28,7 +28,7 @@ const getAllMessages = async (req, res) => {
 }
 
 const getNewMessages = async (req, res) => {
-    const { lastRequest } = req.headers
+    const { lastRequest } = req.query
     const { id: userID } = req.params
 
     let newMessages
